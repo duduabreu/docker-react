860bb02346087efc0cd044ecff6b317ca7c3a3ff
eduardo.fortes <eduardo.fortes@nostrum.com.br>
2022-09-28 19:02:17 -0300

    change app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bye there!!
+         Oi amor da minha vida, lindurinha do meu coração
         </p>
         <a
           className="App-link"
